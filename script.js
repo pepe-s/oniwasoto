e3abd716e25604f52a0763160e2340865e1fa9cf
pepe-s <ta.te.chj.hi.g@gmail.com>
2023-02-01 23:11:06 +0900

    fly mame

---
+++ script.js
@@ -0,0 +1,67 @@
+window.onload = () => {
+  const targetImgEl = document.getElementById("targetImg");
+  targetImgEl.addEventListener("click", () => {
+    console.log("aaa");
+  });
+  const mameImgEl = document.getElementById("mame");
+  mameImgEl.addEventListener("click", () => {
+    console.log("aaa");
+  });
+
+  // const tamaImgEl = document.getElementById("tama");
+  // tamaImgEl.hidden = true;
+
+  let startPoint = 0;
+  let flg = false;
+  mameImgEl.addEventListener("touchstart", (event) => {
+    console.log("start:" + event.touches[0].pageY);
+    startPoint = event.touches[0].pageY;
+    flg = true;
+  });
+  mameImgEl.addEventListener("touchmove", (event) => {
+    const currentPoint = event.touches[0].pageY;
+    const diff = startPoint - currentPoint;
+    if (Math.abs(diff) > 50 && flg) {
+      if (startPoint > currentPoint) {
+        console.log("up");
+        // tamaImgEl.hidden = false;
+        // setStartPosition();
+        doOniwasoto();
+      } else {
+        console.log("down");
+      }
+      flg = false;
+    }
+  });
+};
+
+function setStartPosition() {
+  const mameImgEl = document.getElementById("mame");
+  const tamaImgEl = document.getElementById("tama");
+
+  console.log(tamaImgEl.top);
+  console.log(tamaImgEl.left);
+}
+
+function createTama() {
+  const tama = document.createElement("img");
+  tama.src = "./images/fruit_cacao_kakao.png";
+  tama.width = 20;
+  tama.className = "tama";
+  tama.style.position = "absolute";
+  return tama;
+}
+
+function doOniwasoto() {
+  const mainDiv = document.getElementById("main");
+  const tama = createTama();
+  mainDiv.appendChild(tama);
+  const durationMs = 500;
+  tama.animate(
+    [{ transform: "translateY(-50px)" }, { transform: "translateY(-500px)" }],
+    { fill: "forwards", duration: durationMs }
+  );
+  setTimeout(() => {
+    tama.remove();
+  }, durationMs);
+}
